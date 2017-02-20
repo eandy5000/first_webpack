92d0402f53e4b3f7b1129fb5c1491568fd5dcdee
Andy Eccleston <eandy5000@yahoo.com>
2017-02-20 17:39:54 -0600

    add image compression and sizing

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+    //relative path from our root directory
+    entry: './src/index.js',
+
+    output: {
+    //absolute path from our root directory    
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: 'build/'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/
+            },
+            {
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader'
+                }),
+                test: /\.css$/
+            },
+            {
+                test: /\.(jp?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: { limit: 40000 }
+                    },
+                    'image-webpack-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
+
+module.exports = config;
