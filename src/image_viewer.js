e5e59fcefe7d1c004b74ee5def6acc328b2ba5d2
Andy Eccleston <eandy5000@yahoo.com>
2017-02-21 10:56:19 -0600

    finished basic code-splitting example

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -2,11 +2,19 @@ import '../styles/image_viewer.css';
 import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 
-const image = document.createElement('img');
-const BigImage = document.createElement('img');
+// const image = document.createElement('img');
+// const BigImage = document.createElement('img');
 
-image.src = small;
-BigImage.src = big;
+// image.src = small;
+// BigImage.src = big;
 
-document.body.appendChild(image);
-document.body.appendChild(BigImage);
+// document.body.appendChild(image);
+// document.body.appendChild(BigImage);
+
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
+    
+    document.body.appendChild(image);
+
+}
